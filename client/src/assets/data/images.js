3bd879d2df1b777ed78d94766bddf23d6e941217
Adeeb Ogaili <adeebnasser@gmail.com>
2023-04-14 19:12:42 -0500

    added react Icon

--- client/src/assets/data/images.js
+++ client/src/assets/data/images.js
@@ -1,6 +1,7 @@
 import html5Img from '../images/html5.png';
 import cssImg from '../images/css.png';
 import javascriptImg from '../images/javascript.png';
+import reactImg from '../images/react.png';
 import bootstrapImg from '../images/bootstrap.png';
 import gitImg from '../images/git.png';
 import mongoDBImg from '../images/mongoDB.png';
@@ -9,33 +10,38 @@ const skills = [
   {
     alt: 'HTML5 Logo',
     image: html5Img,
-    url: 'https://www.w3schools.com/html/default.asp'
+    url: 'https://www.w3schools.com/html/default.asp',
   },
   {
     alt: 'CSS Logo',
     image: cssImg,
-    url: 'https://www.w3schools.com/css/default.asp'
+    url: 'https://www.w3schools.com/css/default.asp',
   },
   {
     alt: 'Javascript Logo',
     image: javascriptImg,
-    url: 'https://www.w3schools.com/js/default.asp'
+    url: 'https://www.w3schools.com/js/default.asp',
+  },
+  {
+    alt: 'React Logo',
+    image: reactImg,
+    url: 'https://react.dev/',
   },
   {
     alt: 'Bootstrap Logo',
     image: bootstrapImg,
-    url: 'https://getbootstrap.com'
+    url: 'https://getbootstrap.com',
   },
   {
     alt: 'Git Logo',
     image: gitImg,
-    url: 'https://git-scm.com/'
+    url: 'https://git-scm.com/',
   },
   {
     alt: 'MongoDB Logo',
     image: mongoDBImg,
-    url: 'https://www.mongodb.com/'
+    url: 'https://www.mongodb.com/',
   },
 ];
 
-export default skills
+export default skills;
